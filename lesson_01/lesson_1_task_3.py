1c95e0f48240a8c3cb82d7aaeac33726c943d84a
ser-ulyanov <fourth927@gmail.com>
2026-04-15 21:30:47 +0500

    fix: исправлены ошибки стиля PEP8 (#1)

---
+++ lesson_01/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+frtist_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
+print("Вас зовут: " + frtist_name + " " + last_name + "!")
---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def print_numders(num):
+    print(num, end=' ')
+
+
+print_numders(8)
+print_numders(8)
+print_numders(0)
+print_numders(0)
+print_numders(5)
+print_numders(5)
+print_numders(5)
+print_numders(3)
+print_numders(5)
+print_numders(3)
+print_numders(5)
