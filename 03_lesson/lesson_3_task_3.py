0805fff686ca85e62f4d23fa3415aaf8358574ce
ser-ulyanov <fourth927@gmail.com>
2026-05-08 19:27:06 +0500

    2.0 исправление файлов

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -11,9 +11,3 @@ print(
     f"Mailing from {mailing.from_address} to {mailing.to_address}. "
     f"Cost: ${mailing.cost}. Tracking number: {mailing.track}"
 )
-
-address = Address("625034", "Tyumen", "Energetikov street", "16", "A")
-print(
-    f"Address: {address.zip_code}, {address.city}, {address.street}, "
-    f"{address.house}, {address.apartment}"
-)
