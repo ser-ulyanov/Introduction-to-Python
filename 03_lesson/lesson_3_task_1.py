a1e0561ced34b96c78ad808937bd069fb7b1c063
ser-ulyanov <fourth927@gmail.com>
2026-05-09 17:30:55 +0500

    Корректировки внесены и создан файл .gitignore

--- 03_lesson/lesson_3_task_1.py
+++ 03_lesson/lesson_3_task_1.py
@@ -2,6 +2,6 @@ from user import User
 
 my_user = User("Fiona", "Sherk")
 
-print(my_user.name())
-print(my_user.last_name)
-print(my_user.full_name())
+print(my_user.get_first_name())
+print(my_user.get_last_name())
+print(my_user.get_full_name())
